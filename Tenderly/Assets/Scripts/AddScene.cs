1b70f2af0de373cfe629c9e141db1f4ba921a34b
Brian Davis <briandavism@gmail.com>
2020-08-23 03:32:32 +0200

    Camera Movement and Scene loader

---
+++ Tenderly/Assets/Scripts/AddScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AddScene : MonoBehaviour
+{
+    public void btn_add_scene(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name,LoadSceneMode.Additive);
+        SceneManager.UnloadSceneAsync("menu");
+    }
+}
---
+++ Tenderly/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void btn_change_scene(string scene_name, string ui_scene)
+    {
+        SceneManager.LoadScene(scene_name, LoadSceneMode.Additive);
+    }
+}
