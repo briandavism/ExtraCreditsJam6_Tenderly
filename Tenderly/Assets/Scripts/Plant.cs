bdf836abe98a145a518554ff910f7d436d813c94
Brian Davis <briandavism@gmail.com>
2020-08-31 13:36:12 +0200

    Added Plant class and plants csv importer

---
+++ Tenderly/Assets/Scripts/Plant.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//https://www.youtube.com/watch?v=mAeTRCT0qZg
+
+public class Plant
+{
+    public int id;
+    public string Name;
+    public int Score;
+    public int BarrenDrop;
+    public int SoilDrop;
+    public int MarshDrop;
+    public int WaterDrop;
+    public string Ingredient1;
+    public int Count1;
+    public string Ingredient2;
+    public int Count2;
+}
