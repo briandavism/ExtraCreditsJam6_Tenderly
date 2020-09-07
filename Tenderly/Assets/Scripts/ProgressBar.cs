f663efa5ef8575715cd2bfd14324d0e972fe39df
Brian Davis <briandavism@gmail.com>
2020-09-07 22:10:02 +0200

    Added score and progress bar

---
+++ Tenderly/Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//https://fractalpixels.com/devblog/unity-2D-progress-bars
+
+public class ProgressBar : MonoBehaviour
+{
+    public Slider slider;
+    public Text displayText;
+
+    public static int waterEarned = 0;
+    private int waterXpReq;
+    private int spentWaterXp;
+    private int NewWaterXP;
+
+    private int waterXpBase = 100;
+    private float waterXpExponent = 1.33f;
+
+    // Create a property to handle the slider's value
+    private float currentValue = 0f;
+    public float CurrentValue
+    {
+        get
+        {
+            return currentValue;
+        }
+        set
+        {
+            
+            currentValue = value;
+            slider.value = currentValue;
+            displayText.text = (slider.value * 100).ToString("0.00") + "%";
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentValue = 0f;
+        waterXpReq = (int)Mathf.Round(Mathf.Pow(waterEarned, waterXpExponent) + waterXpBase);
+    }
+
+    void GrantWater()
+    {
+        spentWaterXp += waterXpReq;
+        waterEarned++;
+        waterXpReq = (int)Mathf.Round(Mathf.Pow(waterEarned, waterXpExponent) + waterXpBase);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NewWaterXP = Mathf.Max(Score.scoreCount - spentWaterXp,0);
+        displayText.text = NewWaterXP.ToString() + "/" + waterXpReq;
+        slider.value = (float)NewWaterXP / (float)waterXpReq;
+        if(NewWaterXP >= waterXpReq)
+        {
+            GrantWater();
+        }
+
+        //CurrentValue += 0.0043f;
+    }
+}
---
+++ Tenderly/Assets/Scripts/Score.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text score;
+    public static int scoreCount;
+    private double timeCount = 0.0;
+    private double pointThreshold = 0.1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCount += Time.deltaTime;
+        if(timeCount >= pointThreshold)
+        {
+            timeCount = 0.0;
+            scoreCount++;
+        }
+        score.text = scoreCount.ToString();
+    }
+}
