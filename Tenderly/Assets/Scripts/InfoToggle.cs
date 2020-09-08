efb847e5bd2a81d43d4d691751a095dafa4e7924
Brian Davis <briandavism@gmail.com>
2020-09-08 05:22:50 +0200

    Added credits

---
+++ Tenderly/Assets/Scripts/InfoToggle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoToggle : MonoBehaviour
+{
+    private bool toggle = false;
+
+    public void ToggleUpdate(GameObject image)
+    {
+        toggle = !toggle;
+        if (toggle)
+        {
+            image.SetActive(true);
+        }
+        else if (!toggle)
+        {
+            image.SetActive(false);
+        }
+
+    }
+}
